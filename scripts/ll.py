c14992c47f6c1a9ac0423cf4c4c94e79386b11db
Mavlin Dm <mavlind@list.ru>
2022-12-31 18:13:05 +0700

    set colored perms

--- scripts/ll.py
+++ scripts/ll.py
@@ -1,8 +1,10 @@
+import re
 from typing import Union
 
 import sh
 from logrich.logger_ import log
 from rich.console import Console
+from rich.filesize import decimal
 from rich.table import Table
 from rich.style import Style
 import argparse
@@ -17,11 +19,11 @@ def ll(dps_list: Union[None, str] = None, column_delimiter: str = "~~~", sort: i
     else:
         dps_ = sh.bash(
             "-c",
-            "ls -al",
+            f'ls -al --time-style=long-iso |  sed -r s"/\s+/{column_delimiter}/g"',
+            # f'find . -maxdepth 1 -printf "%M{column_delimiter}%n{column_delimiter}%u{column_delimiter}%g{column_delimiter}%s{column_delimiter}%P\n"',
         )
-        dps_ = list(dps_)
-    log.debug(11)
-    return
+        # dps_ = list(dps_)
+    # log.debug("", o=dps_)
     if sort == 1:
         # сортируем, сохраняем первую строку - заголовок
         first_row = dps_[:1]
@@ -42,21 +44,48 @@ def ll(dps_list: Union[None, str] = None, column_delimiter: str = "~~~", sort: i
         box=None,
     )
 
-    table.add_column("Names", justify="left", style="red", max_width=20)
-    table.add_column("Status", justify="left", style="green", max_width=27)
-    table.add_column("Networks", justify="left", style="magenta", max_width=23)
-    table.add_column("Ports", justify="left", style="yellow")
+    table.add_column("Perms", justify="left", style="cyan", max_width=20)
+    table.add_column("Chld", justify="left", style="green", max_width=27)
+    table.add_column("User", justify="left", style="magenta", max_width=23)
+    table.add_column("Group", justify="left", style="yellow")
+    table.add_column("Size", justify="left", style="green", max_width=27)
+    table.add_column("Date", justify="left", style="green", max_width=27)
+    table.add_column("Time", justify="left", style="green", max_width=27)
+    table.add_column("Icons", justify="left", style="magenta", max_width=23)
+    table.add_column("Content", justify="left", style="yellow", no_wrap=False)
 
+    def get_size(arg: str) -> str:
+        size = decimal(int(arg))
+        return str(size)
+
+    def get_perm(arg: str) -> str:
+        resp = re.sub("r", "[#27C864]r[/]", arg)
+        resp = re.sub("d", "[#27A1C8]d[/]", resp)
+        resp = re.sub("x", "[#A6C827]x[/]", resp)
+        resp = re.sub("w", "[#C87C27]w[/]", resp)
+        resp = re.sub("-", "[#798721]-[/]", resp)
+        return resp
+
+    # return
     for key, val in enumerate(dps_):
         cells = val.split(column_delimiter)
+        # print(cells)
         if key != 0:
             table.add_row(
-                cells[0],
+                get_perm(cells[0]),
                 cells[1],
+                cells[2],
+                cells[3],
+                get_size(cells[4]),
+                cells[5],
+                cells[6],
+                # *cells[:-1],
+                "icon",
+                cells[-1][:-1],
                 # сети слеплены без пробела
-                ", ".join(cells[2].split(",")),
+                # ", ".join(cells[2].split(",")),
                 # последняя клетка может содержать или не содержать символ новой строки
-                (lambda cell: cells[-1][:-1] if cells[-1][-1:] == "\n" else cells[-1])(cells),
+                # (lambda cell: cells[-1][:-1] if cells[-1][-1:] == "\n" else cells[-1])(cells),
                 style=(lambda key_: "on #18181C" if key_ % 2 else "on black")(key),
             )
     console = Console()
